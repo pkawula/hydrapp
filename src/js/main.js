dd52b737f8572ac0c85f57e1a974cf75c989ce50
Piotr Kawula <kontakt@piotrkawula.pl>
2019-07-26 07:44:13 +0200

    Feat: started js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
-"use strict";
-
-console.log('up and running 🚀')
+const textDate = document.querySelector('.day__js'),
+    glass = document.querySelector('.glass__js'),
+    glassCount = document.querySelector('.glass__count--js'),
+    btnPrimary = document.querySelector('btn__primary--js'),
+    btnSecondary = document.querySelector('btn__secondary--js');
